8168c3e3ef43a600553158c5b23aea95dc6b2f6d
Meanle <duanlele7@gmail.com>
2016-11-25 14:20:23 +0800

    雁凯商城服务器copy

--- Apps/Common/Conf/config.php
+++ Apps/Common/Conf/config.php
@@ -5,10 +5,10 @@ return array(
     'VAR_PAGE'=>'p',
     'PAGE_SIZE'=>15,
     'DB_TYPE'=>'mysql',
-    'DB_HOST'=>'localhost',
+    'DB_HOST'=>'192.168.127.1',
     'DB_NAME'=>'wstmall',
     'DB_USER'=>'root',
-    'DB_PWD'=>'slarker.comyk',
+    'DB_PWD'=>'',
     'DB_PREFIX'=>'wst_',
     'DEFAULT_C_LAYER' =>  'Action',
     'DEFAULT_CITY' => '110100',
@@ -16,15 +16,6 @@ return array(
     'DATA_PATH_LEVEL'=>2,
     'SESSION_PREFIX' => 'WSTMALL',
     'COOKIE_PREFIX'  => 'WSTMALL',
-    'LOAD_EXT_CONFIG' => 'wst_config',
-        'tel' => array(
-        'svr_url' => 'http://112.74.76.186:8030/service/httpService/httpInterface.do',
-        'svr_param' => array(
-            'username' => 'JSM41401',
-            'password' => 'a1gz4e4g',
-            'veryCode' => 'bmlcn4r073qh',
-        )
-    ),
-    
+    'LOAD_EXT_CONFIG' => 'wst_config'
 );
 ?>
