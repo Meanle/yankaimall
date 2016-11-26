336e7d98552b5af5bfd02d4e164256b391a13709
Meanle <duanlele7@gmail.com>
2016-11-26 11:14:52 +0800

    购物车和首页功能完善以及美化

--- Apps/Common/Conf/config.php
+++ Apps/Common/Conf/config.php
@@ -1,3 +1,4 @@
+
 <?php
 return array(
     'URL_MODEL' => 0,
@@ -5,7 +6,7 @@ return array(
     'VAR_PAGE'=>'p',
     'PAGE_SIZE'=>15,
     'DB_TYPE'=>'mysql',
-    'DB_HOST'=>'192.168.127.1',
+    'DB_HOST'=>'192.168.1.108',
     'DB_NAME'=>'wstmall',
     'DB_USER'=>'root',
     'DB_PWD'=>'',
@@ -16,6 +17,15 @@ return array(
     'DATA_PATH_LEVEL'=>2,
     'SESSION_PREFIX' => 'WSTMALL',
     'COOKIE_PREFIX'  => 'WSTMALL',
-    'LOAD_EXT_CONFIG' => 'wst_config'
+    'LOAD_EXT_CONFIG' => 'wst_config',
+    'tel' => array(
+        'svr_url' => 'http://112.74.76.186:8030/service/httpService/httpInterface.do',
+        'svr_param' => array(
+            'username' => 'JSM41401',
+            'password' => 'a1gz4e4g',
+            'veryCode' => 'bmlcn4r073qh',
+        )
+    ),
+
 );
 ?>
